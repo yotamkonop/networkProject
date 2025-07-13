64c02df08742657526cd524736a2819fe797813d
yotamkonop <yotamkonop@gmail.com>
2025-07-13 23:46:27 +0300

    full implementation

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import threading
 import queue
 from GUI import run_gui
-#from network import listen
+from network import listen
+from network import send
 
 
 def main():
     msg_queue = queue.Queue()
 
     # Start network thread only
-  #  threading.Thread(target=listen_network, args=(msg_queue,), daemon=True).start()
+    threading.Thread(target=listen, args=(msg_queue,), daemon=True).start()
 
     # GUI must run on main thread
-    run_gui(msg_queue)
+    run_gui(msg_queue, send)
 
 
 if __name__ == "__main__":
